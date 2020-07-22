f47f413d20cafc30a575c92473db8e3d04d922db
cc <ziyoushigv@163.com>
2020-07-22 16:47:54 +0800

    Initial commit

---
+++ src/com/develop/data/structure/Demo1.java
@@ -0,0 +1,11 @@
+package com.develop.data.structure;
+
+/**
+ * @author changchen
+ * @create 2020-07-22 16:46
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
