c90e83c1579a232ea8995a5516f46d5d637a9f7d
cc <ziyoushigv@163.com>
2020-07-26 10:55:54 +0800

    链表的分析

---
+++ src/com/develop/data/structure/linkedlist/LinkedListDemo.java
@@ -0,0 +1,19 @@
+package com.develop.data.structure.linkedlist;
+
+/**
+ * @author changchen
+ * @create 2020-07-26 9:25
+ * 链表是有序的列表
+ */
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+        /**
+         * 小结：
+         *  1、链表是以节点的方式存储;
+         *  2、每个节点包含data域(存储数据)，next域指向下一个节点;
+         *  3、链表的各个节点不一定是连续存放的;
+         *  4、链表分带头节点的链表和没有头节点的链表，根据实际需求来确定;
+         */
+    }
+}
